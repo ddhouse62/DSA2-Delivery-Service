931718cd1c404f7c7b3cea0285431d6b25facc60
Dalton House <house.dalton62@gmail.com>
2025-06-03 21:33:33 -0500

    Adjust/fix hashtable class

--- hash_table.py
+++ hash_table.py
@@ -21,39 +21,43 @@ class HashTable:
     def insert(self, key, item):
 
         # Use hash function defined previously to identify index of correct bucket
-        index = self.hash(key)
+        bucket = self.hash(key)
+
 
         # For each key-item pair, if key exists, update current item
-        for kv in self.table[index]:
+        for kv in self.table[bucket]:
             if kv[0] == key:
                 kv[1] = item
                 return
 
         # If key does not exist, append key-item pair to bucket-list
-        self.table[index].append(key, item)
+        kv = [key, item]
+        self.table[bucket].append(kv)
+
 
 
     # Implement search function with key as parameter
     def search(self, key):
 
-        # Identify index of correct bucket
-        index = self.hash(key)
-
-        # Iterate through bucket-list to find item
-        for pair in self.table[index]:
-            if pair[0] == key:
-                return pair[1]
-        # Return None if key does not exist in index
+        # identify index of correct bucket
+        bucket = self.hash(key)
+        # Return value if key exists in bucket
+        print(self.table[bucket])
+        for kv in self.table[bucket]:
+            if kv[0] == key:
+                return kv[1]
+        # Return None if key does not exist in bucket
         return None
 
     def delete(self, key):
-        # Use search function to identify correct index of bucket
-        index = self.hash(key)
+        # identify item to remove
+        bucket = self.hash(key)
 
         # Find and delete item - return if found
-        for i in range(self.table[index]):
-            if self.table[index][i][0] == key:
-                del(self.table[index][i])
+        for kv in self.table[bucket]:
+            if kv[0] == key:
+                self.table[bucket].remove(kv)
+                print(f"Item {key} removed from table")
                 return
         # Indicate to user that item was not found with given key
-        print("Key not found")
+        print("key not found")
